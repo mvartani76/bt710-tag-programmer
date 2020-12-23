bdb5dd7abba0c3ec06d72ba917ee080ab35876cf
Mike Vartanian <mike.vartanian@gmail.com>
2020-12-23 17:00:45 -0500

    Added batch view controller to handle the batch operations of programming and registering tags.

---
+++ iOS/bt710-programmer/bt710-programmer/BatchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BatchViewController.swift
+//  bt710-programmer
+//
+//  Created by Michael Vartanian on 12/23/20.
+//
+
+import UIKit
+import CoreBluetooth
+
+class BatchViewController: UIViewController {
+    
+}
