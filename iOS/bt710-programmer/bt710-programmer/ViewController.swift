e7a114e0104bef9d26cb5180a3e4a36a5b6e9bd6
Mike Vartanian <mike.vartanian@gmail.com>
2020-12-23 10:52:53 -0500

    Initial add of XCode project restricting to portrait mode only and changing deployment target to 13.3 to match my iPhone.

---
+++ iOS/bt710-programmer/bt710-programmer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bt710-programmer
+//
+//  Created by Michael Vartanian on 12/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
